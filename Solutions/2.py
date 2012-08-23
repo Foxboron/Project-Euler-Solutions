89f52a173d6221f3fcb551b6d460d7d33f0772cc
Foxboron <mcfoxax@gmail.com>
2012-08-23 17:38:18 +0200

    Removed a loop

--- Solutions/2.py
+++ Solutions/2.py
@@ -4,16 +4,13 @@
 def main():
     a = 1
     b = 2
-    listnum = []
     listsum = []
     while True:
-        listnum.append(a)
         a, b = b, a + b
         if a >= 4000000:
             break
-    for i in listnum:
-        if i % 2 == 0:
-            listsum.append(i)
+        if a % 2 == 0:
+            listsum.append(a)
     print "Answer: %s" % (sum(listsum))
 
 if __name__ == '__main__':
