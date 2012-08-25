1809a6bfb20e2cbe7b43c06dfaf420845ecac6ec
Foxboron <mcfoxax@gmail.com>
2012-08-26 01:56:40 +0200

    Added solution for problem 9.

---
+++ Solutions/9.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from math import sqrt
+
+def main():
+    n  = 1000
+    for a in range(1, 500):
+        for b in range(1, 500):
+            c = pow(a,2) + pow(b,2)
+            c = sqrt(c)
+            if pow(a,2) + pow(b,2) == pow(c,2) and a + b + c == n:
+                print a*b*c
+                print a
+                print b
+                print c
+                break
+
+if __name__ == '__main__':
+    main()
